db6b5cb2861b540a9a0ad8a52940b0e09148ced0
NRoseDev <nicholer001@gmail.com>
2026-08-08 13:19:28 -0700

    Add RiseAndAlignHeader component

---
+++ RiseAndAlignHeader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function RiseAndAlignHeader() {
+  return (
+    <div className="text-center my-8">
+      <h1 className="text-4xl font-bold tracking-wide">Rise & Align.</h1>
+      <p className="text-lg opacity-80 mt-2">Here are three manifestations ready for your review.</p>
+    </div>
+  );
+}
